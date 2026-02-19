2d2f3afebbe872add4788c7af371a5bcdec1be59
Shishupal kumar <shishupal@Shishupals-MacBook-Air.local>
2026-02-19 23:49:57 +0530

    remove jpa line prom property

--- src/main/java/com/shishupal/chatapp/config/SecurityConfig.java
+++ src/main/java/com/shishupal/chatapp/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.shishupal.chatapp.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
