0452c1674992913c2b46583bd37719bd23ac98dd
Shishupal kumar <shishupal@Shishupals-MacBook-Air.local>
2026-02-23 21:38:41 +0530

    refined

--- src/main/java/com/shishupal/chatapp/dto/TypingDTO.java
+++ src/main/java/com/shishupal/chatapp/dto/TypingDTO.java
@@ -4,18 +4,11 @@ public class TypingDTO {
 
     private String receiver;
 
-    public TypingDTO() {
-    }
-
     public TypingDTO(String receiver) {
         this.receiver = receiver;
     }
 
     public String getReceiver() {
         return receiver;
-    }
-
-    public void setReceiver(String receiver) {
-        this.receiver = receiver;
     }
 }
