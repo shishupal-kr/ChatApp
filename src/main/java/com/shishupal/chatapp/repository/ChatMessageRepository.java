1126884ff0f2a642b86efe3e6148952201a25546
shishupal <shishupalking@gmail.com>
2026-03-08 20:47:16 +0530

    fix repo file annotation

--- src/main/java/com/shishupal/chatapp/repository/ChatMessageRepository.java
+++ src/main/java/com/shishupal/chatapp/repository/ChatMessageRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
  // used for CRUD operations on ChatMessageEntity entity
+@Repository
 public interface ChatMessageRepository
         extends JpaRepository<ChatMessageEntity, Long> {
 
--- src/main/java/com/shishupal/chatapp/repository/UserRepository.java
+++ src/main/java/com/shishupal/chatapp/repository/UserRepository.java
@@ -4,11 +4,14 @@ import com.shishupal.chatapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 import java.util.Optional;
 
 // used for CRUD operations on User entity
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByUsername(String username);
