0fc0045d64e679d77b8b8fedf882b13da096652a
shishupal <shishupalking@gmail.com>
2026-04-22 11:29:51 +0530

    incr. token time

--- src/main/java/com/shishupal/chatapp/service/JwtService.java
+++ src/main/java/com/shishupal/chatapp/service/JwtService.java
@@ -24,7 +24,7 @@ public class JwtService {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 26))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
